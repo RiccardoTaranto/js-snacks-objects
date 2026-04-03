2b18848c3ac8ff1ce768b4c3953e8e9416ad62a6
Riccardo <riccardo.taranto@icloud.com>
2026-04-03 10:57:28 +0200

    Add array of bike objects with names and weights

--- main.js
+++ main.js
@@ -7,3 +7,29 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare a schermo la bici con peso minore.
 */
 
+const bikes = {
+    bike_1: {
+        name: 'Bianchi',
+        weight: 10
+    },
+
+    bike_2: {
+        name: 'Atala',
+        weight: 12
+    },
+
+    bike_3: {
+        name: 'Colnago',
+        weight: 8
+    },
+
+    bike_4: {
+        name: 'Pinarello',
+        weight: 9
+    },
+
+    bike_5: {
+        name: 'De Rosa',
+        weight: 11
+    }
+}
